32f15a0228dba918ee55b2967f4d6d28af33d5e0
relicarioenformadecorazon <76561198000@protonmail.com>
2023-03-28 16:27:23 -0600

    initial commit

---
+++ screenshot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#scrot -d 1 -q $HOME/i3BlocksScripts/
+
+scrot -q 50 '/home/void/i3BlocksScripts/filename_%m %d %T.png'
---
+++ wpanetwork.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wpa_supplicant -B -i wlp1s0f0u2 -c /etc/wpa_supplicant/wpa_supplicant.conf
